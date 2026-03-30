4d89120cbd263f858cd7c34e0e938a0d92c88da4
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-30 09:39:43 -0700

    Write party::party tests

--- tests/party_test.cpp
+++ tests/party_test.cpp
@@ -1,7 +1,37 @@
 #include <gtest/gtest.h>
 #include <monlib/party.hpp>
 
+#include <monlib/mon.hpp>
+#include <vector>
 
-TEST(PartyTest, CanCreatePartyObject) {
+
+class PartyFixture : public ::testing::Test {
+    protected:
+        void SetUp() override {
+            party = monlib::party();
+        }
+
+        void TearDown() override {
+
+        }
+
+        monlib::party party;
+};
+
+
+/* party::party */
+TEST(PartyTest, CanConstructPartyObject) {
     monlib::party party;
+}
+
+
+TEST(PartyTest, CanConstructPartyObjectFromParameters) {
+    std::vector<monlib::mon> mons;
+    monlib::party party(mons);
+}
+
+
+TEST(PartyTest, CanConstructPartyObjectFromReference) {
+    std::vector<monlib::mon> mons;
+    monlib::party(monlib::party(mons));
 }
