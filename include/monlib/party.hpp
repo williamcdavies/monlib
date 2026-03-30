c0759226910bb0fab12690439e14cccc2bad2c4d
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-03-30 09:39:56 -0700

    Resolve party::party tests

--- include/monlib/party.hpp
+++ include/monlib/party.hpp
@@ -1,12 +1,22 @@
 #ifndef PARTY_HPP
 #define PARTY_HPP
 
+#include <monlib/mon.hpp>
+#include <vector>
+
 
 namespace monlib {
 
 
 class party {
-    
+    private:
+        std::vector<monlib::mon> mons_;
+
+    public:
+        /* party::party */
+        party() = default;
+        party(const std::vector<monlib::mon>& mons);
+
 };
 
 
--- src/party.cpp
+++ src/party.cpp
@@ -1,1 +1,14 @@
 #include <monlib/party.hpp>
+
+#include <monlib/mon.hpp>
+#include <vector>
+
+
+namespace monlib {
+
+
+    /* party::party */
+    party::party(const std::vector<monlib::mon>& mons) { this->mons_ = mons; }
+
+
+} // namespace monlib
