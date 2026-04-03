fe1656a0b805f1f1b5a678c4b501b78bc485221e
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-04-02 19:31:58 -0700

    Add time component to make_temporary

--- tests/utils/filesystem.hpp
+++ tests/utils/filesystem.hpp
@@ -1,6 +1,7 @@
 #ifndef FILESYSTEM_HPP
 #define FILESYSTEM_HPP
 
+#include <chrono>
 #include <filesystem>
 #include <stdexcept>
 #include <string>
@@ -10,8 +11,13 @@
 inline std::filesystem::path make_temporary(const std::filesystem::path& source) {
     if (!std::filesystem::is_regular_file(source)) throw std::runtime_error("Path '" + source.string() + "' is not a regular file");
 
-    std::string           tmp_filename{ "tmp_" + source.filename().string()                   };
-    std::filesystem::path tmp_file    { std::filesystem::temp_directory_path() / tmp_filename };
+    std::string           tmp_filename{ "tmp_" + 
+                                        source.stem().string() + 
+                                        "_" + 
+                                        std::to_string(std::chrono::high_resolution_clock::now().time_since_epoch().count()) + 
+                                        "_" + 
+                                        source.extension().string() };
+    std::filesystem::path tmp_file    { std::filesystem::temp_directory_path() / tmp_filename                                                                                                            };
     
     std::filesystem::copy_file(source, tmp_file, std::filesystem::copy_options::overwrite_existing);
 
